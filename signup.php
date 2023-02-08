a1cf2b025094d52e769b86be73ecd7db3821da0e
BimaaTio <bimaatio10@gmail.com>
2023-02-08 09:28:22 +0700

    terst

--- signup.php
+++ signup.php
@@ -42,60 +42,26 @@ require 'config/functions.php';
               <div class="row mb-4">
                 <div class="col">
                   <div class="form-outline">
-                    <input type="text" id="form3Example1" class="form-control" />
-                    <label class="form-label" for="form3Example1">First name</label>
+                    <label class="form-label" for="form3Example1">Nama Lengkap</label>
+                    <input type="text" id="form3Example1" name="fn" class="form-control" />
                   </div>
                 </div>
-                <div class="col">
-                  <div class="form-outline">
-                    <input type="text" id="form3Example2" class="form-control" />
-                    <label class="form-label" for="form3Example2">Last name</label>
-                  </div>
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="form3Example3">Email address</label>
+                  <input type="email" id="form3Example3" name="email" class="form-control" />
                 </div>
-              </div>
-
-              <!-- Email input -->
-              <div class="form-outline mb-4">
-                <input type="email" id="form3Example3" class="form-control" />
-                <label class="form-label" for="form3Example3">Email address</label>
-              </div>
-
-              <!-- Password input -->
-              <div class="form-outline mb-4">
-                <input type="password" id="form3Example4" class="form-control" />
-                <label class="form-label" for="form3Example4">Password</label>
-              </div>
-
-              <!-- Checkbox -->
-              <div class="form-check d-flex justify-content-center mb-4">
-                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
-                <label class="form-check-label" for="form2Example33">
-                  Subscribe to our newsletter
-                </label>
-              </div>
-
-              <!-- Submit button -->
-              <button type="submit" class="btn btn-primary btn-block mb-4">Sign up</button>
-
-              <!-- Register buttons -->
-              <div class="text-center">
-                <p>or sign up with:</p>
-                <button type="button" class="btn btn-secondary btn-floating mx-1">
-                  <i class="fab fa-facebook-f"></i>
-                </button>
-
-                <button type="button" class="btn btn-secondary btn-floating mx-1">
-                  <i class="fab fa-google"></i>
-                </button>
-
-                <button type="button" class="btn btn-secondary btn-floating mx-1">
-                  <i class="fab fa-twitter"></i>
-                </button>
-
-                <button type="button" class="btn btn-secondary btn-floating mx-1">
-                  <i class="fab fa-github"></i>
-                </button>
-              </div>
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="form3Example4">Password</label>
+                  <input type="password" id="form3Example4" name="pass" class="form-control" />
+                </div>
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="form3Example4">Konfirmasi Password</label>
+                  <input type="password" id="form3Example4" name="pass2" class="form-control" />
+                </div>
+                <!-- Submit button -->
+                <button type="submit" name="submit" class="btn btn-primary btn-block mb-4">Sign up</button>
             </form>
           </div>
         </div>
